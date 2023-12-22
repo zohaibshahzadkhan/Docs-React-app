a46f042675532311960cd30e426dbb452dd83c85
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2023-12-22 18:51:37 +0100

    :sparkles: Add foreground component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Background from './components/Background'
+import Foreground from './components/Foreground'
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-900'>
       <Background />
+      <Foreground />
     </div>
   )
 }
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Foreground() {
+    return (
+        <div className='fixed z-[3] w-full h-full bg-sky-800 bg-transparent' ></div>
+    )
+}
+
+export default Foreground
